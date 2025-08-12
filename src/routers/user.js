a3f1912e7ef860d6de6fc4135e278c7f8be2400d
Kaiky <2004kfs@gmail.com>
2025-08-12 13:11:12 -0300

    added a login endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,10 +9,20 @@ router.post('/users', async (req, res) => {
     try {
         await user.save()
         res.status(201).send(user)
-    } catch (error) {
-        res.status(400).send(error)
+    } catch (e) {
+        res.status(400).send(e)
     }
 });
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 router.get('/users', async (req, res) => {
     try {
