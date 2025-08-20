2bdea3792bfd2adaa163d9e2f1faf90d5c0f1f3a
Kaiky <2004kfs@gmail.com>
2025-08-20 13:39:37 -0300

    path changes

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import './db/mongoose.js'
+import userRouter from '../api/routers/user.js'
+import taskRouter from '../api/routers/task.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+export default app
